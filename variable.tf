5b758414e55c978b8178b1e3a598e2b76f4f5689
dewshekhar <45204261+dewshekhar@users.noreply.github.com>
2022-08-05 20:39:54 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,21 @@
+locals {
+  app_name = "${var.app_name}-${var.env}"
+}
+variable "app_name" {
+}
+variable "pub_sub_cidr_1" {
+}
+variable "pub_sub_cidr_2" {
+}
+variable "pri_sub_cidr_1" {
+}
+variable "pri_sub_cidr_2" {
+}
+variable "vpc_cidr" {
+}
+variable "env" {
+}
+variable "availability_zone_1" {
+}
+variable "availability_zone_2" {
+}
